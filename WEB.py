5b6b4f4af393e9fb4ddc768125f9de445da9da49
VendettaBo1 <99539107+VendettaBo1@users.noreply.github.com>
2022-03-05 15:23:01 +0300

    Update WEB.py

--- WEB.py
+++ WEB.py
@@ -22,9 +22,20 @@ def user_input_features():
     features = pd.DataFrame(data, index=[0])
     return features
 
+df = user_input_features()
 
 st.subheader('User Input parameters')
 st.write(df)
+
+iris = datasets.load_iris()
+X = iris.data
+Y = iris.target
+
+clf = RandomForestClassifier()
+clf.fit(X, Y)
+
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
 st.write(iris.target_names)
