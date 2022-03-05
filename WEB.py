5937fa822ea5661c2eba0468592bf6c68801f91f
Asphyxia777 <92206572+Asphyxia777@users.noreply.github.com>
2022-03-05 15:03:24 +0300

    Update WEB.py

--- WEB.py
+++ WEB.py
@@ -3,10 +3,7 @@ import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
-st.write("""
-# Simple Iris Flower Prediction App
-This app predicts the **Iris flower** type!
-""")
+
 
 st.sidebar.header('User Input Parameters')
 
@@ -22,27 +19,3 @@ def user_input_features():
     features = pd.DataFrame(data, index=[0])
     return features
 
-df = user_input_features()
-
-st.subheader('User Input parameters')
-st.write(df)
-
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
-
-clf = RandomForestClassifier()
-clf.fit(X, Y)
-
-prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
-
-st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
-
-st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
